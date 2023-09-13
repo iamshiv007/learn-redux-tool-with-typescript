b10e0d80afe83d709e2c6a73a9988997508e7002
iamshiv007 <iamshiv20032003@gmail.com>
2023-09-13 14:51:30 +0530

    first : project initialized learn redux-tool

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import {} from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        Project initialized ( React project with typescript to learn
+        redux-toolkit with typescript )
+      </div>
+    </>
+  );
+}
+
+export default App;
